ad018cb806ce08367d8c36081b271eaffe3a65f8
Sam010000101 <reposme@outlook.com>
2025-08-25 11:32:22 +0100

    Initial commit

---
+++ src/components/OrbitingImages.tsx
@@ -0,0 +1,218 @@
+export default function OrbitingImages() {
+  const angles = ["0deg"]; // Define angles dynamically
+
+  return (
+    <>
+      <div className="marketing-template eds-g-group eds-l-sn-pad-bot-8 eds-l-sm-pad-bot-8 marketing-template--reverse-horizontal">
+        <div className="eds-g-cell eds-g-cell-12-12 eds-g-cell-sw-6-12 eds-g-cell--has-overflow">
+          <div style={{ position: "relative" }}>
+            <img
+              className="eds-max-img"
+              alt="Build apps for 700,000 event creators"
+              src="https://cdn.evbstatic.com/s3-build/fe/build/images/f7409536e0bd3df2a94b3139b17e93cc-radial.svg"
+              style={{ width: "100%", display: "block" }}
+              data-airgap-id="19"
+            />
+            <img
+              className="eds-max-img"
+              alt="Build apps for 700,000 event creators"
+              src="https://cdn.evbstatic.com/s3-build/fe/build/images/f7af7a221c46f8e2eb2106f9185655a1-eventbritecircle_logo.svg"
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "16%",
+                maxWidth: "16%",
+              }}
+              data-airgap-id="20"
+            />
+            <div
+              className="Plx Plx--active Plx--in Plx--in-0 develop-parallax"
+              style={{
+                position: "absolute",
+                inset: "-5%",
+                width: "110%",
+                transformOrigin: "50% 50%",
+                willChange: "transform",
+                transform: "rotate(87.8995deg)",
+              }}
+            >
+              <div
+                className="Plx Plx--active Plx--in Plx--in-0 "
+                style={{
+                  position: "absolute",
+                  top: "26%",
+                  left: "37%",
+                  transformOrigin: "50% 50%",
+                  width: "14.5%",
+                  maxWidth: "14.5%",
+                  willChange: "transform",
+                  transform: "rotate(-87.1966deg)",
+                }}
+              >
+                <img
+                  className="eds-max-img"
+                  alt="Build apps for 700,000 event creators"
+                  src="https://cdn.evbstatic.com/s3-build/fe/build/images/787f873cb97486016b10423fd682c2b7-spotify_logo.svg"
+                  style={{ width: "100%", maxWidth: "100%" }}
+                  data-airgap-id="12"
+                />
+              </div>
+              <div
+                className="Plx Plx--active Plx--in Plx--in-0 "
+                style={{
+                  position: "absolute",
+                  top: "60.5%",
+                  left: "39%",
+                  transformOrigin: "50% 50%",
+                  width: "13.5%",
+                  maxWidth: "13.5%",
+                  willChange: "transform",
+                  transform: "rotate(-87.1966deg)",
+                }}
+              >
+                <img
+                  className="eds-max-img"
+                  alt="Build apps for 700,000 event creators"
+                  src="https://cdn.evbstatic.com/s3-build/fe/build/images/6dfe2a92b78c03a9485e5073f7cdbf05-facebook_logo.svg"
+                  style={{ width: "100%", maxWidth: "100%" }}
+                  data-airgap-id="13"
+                />
+              </div>
+            </div>
+            <div
+              className="Plx Plx--active Plx--in Plx--in-0 "
+              style={{
+                position: "absolute",
+                inset: "-5%",
+                width: "110%",
+                transformOrigin: "50% 50%",
+                willChange: "transform",
+                transform: "rotate(-87.1966deg)",
+              }}
+            >
+              <div
+                className="Plx Plx--active Plx--in Plx--in-0 "
+                style={{
+                  position: "absolute",
+                  top: "64%",
+                  left: "65%",
+                  transformOrigin: "50% 50%",
+                  width: "13.5%",
+                  maxWidth: "13.5%",
+                  willChange: "transform",
+                  transform: "rotate(87.8995deg)",
+                }}
+              >
+                <img
+                  className="eds-max-img"
+                  alt="Build apps for 700,000 event creators"
+                  src="https://cdn.evbstatic.com/s3-build/fe/build/images/2a7ca844268fe5d43a5431512a85f583-bandsintown_logo.svg"
+                  style={{ width: "100%", maxWidth: "100%" }}
+                  data-airgap-id="14"
+                />
+              </div>
+              <div
+                className="Plx Plx--active Plx--in Plx--in-0 "
+                style={{
+                  position: "absolute",
+                  top: "46%",
+                  left: "13%",
+                  transformOrigin: "50% 50%",
+                  width: "13.5%",
+                  maxWidth: "13.5%",
+                  willChange: "transform",
+                  transform: "rotate(87.8995deg)",
+                }}
+              >
+                <img
+                  className="eds-max-img"
+                  alt="Build apps for 700,000 event creators"
+                  src="https://cdn.evbstatic.com/s3-build/fe/build/images/bbc8a6caf1b6aed82fb7535ac6667b12-funbox_logo.svg"
+                  style={{ width: "100%", maxWidth: "100%" }}
+                  data-airgap-id="15"
+                />
+              </div>
+              <div
+                className="Plx Plx--active Plx--in Plx--in-0 "
+                style={{
+                  position: "absolute",
+                  top: "18%",
+                  left: "61%",
+                  transformOrigin: "50% 50%",
+                  width: "13.5%",
+                  maxWidth: "13.5%",
+                  willChange: "transform",
+                  transform: "rotate(87.8995deg)",
+                }}
+              >
+                <img
+                  className="eds-max-img"
+                  alt="Build apps for 700,000 event creators"
+                  src="https://cdn.evbstatic.com/s3-build/fe/build/images/db604e25d17d4c7ce8ab53de957a6364-toneden_logo.svg"
+                  style={{ width: "100%", maxWidth: "100%" }}
+                  data-airgap-id="16"
+                />
+              </div>
+            </div>
+            <div
+              className="Plx Plx--active Plx--in Plx--in-0 "
+              style={{
+                position: "absolute",
+                inset: "-5%",
+                width: "110%",
+                transformOrigin: "50% 50%",
+                willChange: "transform",
+                transform: "rotate(87.8995deg)",
+              }}
+            >
+              <div
+                className="Plx Plx--active Plx--in Plx--in-0 "
+                style={{
+                  position: "absolute",
+                  top: "31%",
+                  left: "84%",
+                  transformOrigin: "50% 50%",
+                  width: "13.5%",
+                  maxWidth: "13.5%",
+                  willChange: "transform",
+                  transform: "rotate(-87.1966deg)",
+                }}
+              >
+                <img
+                  className="eds-max-img"
+                  alt="Build apps for 700,000 event creators"
+                  src="https://cdn.evbstatic.com/s3-build/fe/build/images/5550e69a68e53d4d036116e739886818-radario_logo.svg"
+                  style={{ width: "100%", maxWidth: "100%" }}
+                  data-airgap-id="17"
+                />
+              </div>
+              <div
+                className="Plx Plx--active Plx--in Plx--in-0 "
+                style={{
+                  position: "absolute",
+                  top: "81%",
+                  left: "21%",
+                  transformOrigin: "50% 50%",
+                  width: "14%",
+                  maxWidth: "14%",
+                  willChange: "transform",
+                  transform: "rotate(-87.1966deg)",
+                }}
+              >
+                <img
+                  className="eds-max-img"
+                  alt="Build apps for 700,000 event creators"
+                  src="https://cdn.evbstatic.com/s3-build/fe/build/images/3c1e5d85c28ba59128ce384167862ca8-mailchimp_logo.svg"
+                  style={{ width: "100%", maxWidth: "100%" }}
+                  data-airgap-id="18"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
